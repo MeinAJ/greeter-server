ad38bc9a15f2b853f472b3938338b3dd30f4849f
aj <15102356906@163.com>
2026-02-27 16:38:34 +0800

    push all

---
+++ internal/logic/sayhellologic.go
@@ -0,0 +1,29 @@
+package logic
+
+import (
+	"context"
+
+	"greeter-server/greeter"
+	"greeter-server/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type SayHelloLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewSayHelloLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SayHelloLogic {
+	return &SayHelloLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *SayHelloLogic) SayHello(in *greeter.SayHelloReq) (*greeter.SayHelloResp, error) {
+	// todo: add your logic here and delete this line
+	return &greeter.SayHelloResp{Message: "Hello " + in.Name}, nil
+}
